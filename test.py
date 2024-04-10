8f8ad80f043948ce5c186ead85a2bb27cf8e88aa
chansushin <chansu@gmail.com>
2024-04-10 10:32:35 -0700

    feat: first version for test.py

---
+++ test.py
@@ -0,0 +1,6 @@
+# steps to push the code to git
+# 1. stage the file (click +) - cmd: git add test.py
+# 2. commit the file + comment (click tick) - cmd: git commit -m "message"
+# 3. push the file to git - cmd: git push origin main
+
+print("This is the test.py file to test the git commands")
